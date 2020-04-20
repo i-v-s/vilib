2770d1d3641add9df9e407de53f1f683f510c510
Igor Smirnov <smirnoviv@rambler.ru>
2020-04-20 21:42:29 +0500

    Remove Python bindings; Allow static build

--- visual_lib/python/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='vilib',
-      ext_modules=[cpp_extension.CppExtension('vilib_cpp', ['src/vilib.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
